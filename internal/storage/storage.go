d89e60edf3aec4492485b6a5154302abf353712a
Dorrrke <dorrrke@yandex.ru>
2024-03-14 11:24:56 +0300

    create service logic

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/Dorrrke/GophKeeper/internal/domain/models"
 	"github.com/mattn/go-sqlite3"
 )
 
@@ -20,6 +21,44 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveUser(ctx context.Context, user models.UserModel) (int64, error) {
+	stmt, err := s.db.Prepare("INSERT INTO users(login, hash) VALUES(?,?)")
+	if err != nil {
+		return 0, err
+	}
+	res, err := stmt.ExecContext(ctx, user.Login, user.Hash)
+	if err != nil {
+		var sqliteErr sqlite3.Error
+		if errors.As(err, &sqliteErr) && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("user alredy exists: %w", err)
+		}
+		return 0, err
+	}
+	cID, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return cID, nil
+}
+
+func (s *Storage) GetUserHash(ctx context.Context, login string) (string, error) {
+	stmt, err := s.db.Prepare("SELECT hash FROM users WHERE login = ?")
+	if err != nil {
+		return "", err
+	}
+	row := stmt.QueryRowContext(ctx, login)
+	var hash string
+	err = row.Scan(&hash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", fmt.Errorf("user not found: %w", err)
+		}
+		return "", err
+	}
+	return hash, nil
 }
 
 func (s *Storage) SaveCard(ctx context.Context, name, number, date string, cvv int) (int64, error) {
@@ -54,7 +93,7 @@ func (s *Storage) SaveLogin(ctx context.Context, name, login, password string) (
 	if err != nil {
 		var sqliteErr sqlite3.Error
 		if errors.As(err, &sqliteErr) && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
-			return 0, fmt.Errorf("card alredy exists: %w", err)
+			return 0, fmt.Errorf("login alredy exists: %w", err)
 		}
 		return 0, err
 	}
@@ -76,7 +115,7 @@ func (s *Storage) SaveText(ctx context.Context, name, data string) (int64, error
 	if err != nil {
 		var sqliteErr sqlite3.Error
 		if errors.As(err, &sqliteErr) && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
-			return 0, fmt.Errorf("card alredy exists: %w", err)
+			return 0, fmt.Errorf("text alredy exists: %w", err)
 		}
 		return 0, err
 	}
--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    uId INTEGER PRIMARY KEY,
+    login TEXT,
+    hash TEXT
+);
+CREATE INDEX IF NOT EXISTS idx_login ON users (login);
+
 CREATE TABLE IF NOT EXISTS cards (
     cId INTEGER PRIMARY KEY, 
     name TEXT, 
@@ -5,19 +12,26 @@ CREATE TABLE IF NOT EXISTS cards (
     date TEXT, 
     cvv INTEGER
 );
+CREATE INDEX IF NOT EXISTS idx_card_name ON cards (name);
+
 CREATE TABLE IF NOT EXISTS logins (
     lId INTEGER PRIMARY KEY, 
     name TEXT, 
     login TEXT, 
     password TEXT
 );
+CREATE INDEX IF NOT EXISTS idx_login_name ON logins (name);
+
 CREATE TABLE IF NOT EXISTS text_data (
     tId INTEGER PRIMARY KEY, 
     name TEXT, 
     data TEXT
 );
+CREATE INDEX IF NOT EXISTS idx_text_name ON text_data (name);
+
 CREATE TABLE IF NOT EXISTS binares_data (
     bId INTEGER PRIMARY KEY, 
     name TEXT, 
     data TEXT
 );
+CREATE INDEX IF NOT EXISTS idx_binares_name ON binares_data (name);
